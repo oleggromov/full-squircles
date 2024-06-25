0d6bd517491730cffa5754de5d12034f6fcacb17
Oleg Gromov <mail@oleggromov.com>
2024-06-25 13:12:32 +0100

    Fix everything

--- dev/init.ts
+++ dev/init.ts
@@ -1,7 +1,7 @@
 
 (async function() {
   if ('paintWorklet' in window.CSS) {
-    await (window.CSS as any).paintWorklet.addModule('/src/squircle.js');
+    await (window.CSS as any).paintWorklet.addModule('/src/squircles.js');
     document.body.classList.add('squircles-on');
   }
 })()
